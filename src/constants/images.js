62bdcee4a0ca604a4fcbdf2650a5a9d90df851c6
Md Fahad <fahadbdctglh8@gmail.com>
2023-07-03 10:59:06 +0600

    Created Article Section

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,13 @@
 import Logo from "../assets/Logo.svg";
 import HeroImage from "../assets/Hero.svg";
+import PostImage from "../assets/Posts/post1.jpg";
+import PostProfileImage from "../assets/Posts/post-profile.svg";
 
 const images = {
   Logo,
   HeroImage,
+  PostImage,
+  PostProfileImage,
 };
 
 export default images;
--- src/pages/home/container/Hero.jsx
+++ src/pages/home/container/Hero.jsx
@@ -7,15 +7,15 @@ const Hero = () => {
   return (
     <section className="container mx-auto flex flex-col px-5 py-5 lg:flex-row">
       <div className="mt-10 lg:w-1/2">
-        <h1 className="font-roboto text-3xl text-center font-bold text-dark-soft md:text-5xl lg:text-left lg:max-w-[540px]">
+        <h1 className="font-roboto text-3xl text-center font-bold text-dark-soft md:text-5xl lg:text-4xl xl:text-5xl lg:text-left lg:max-w-[540px]">
           Read the most interesting articles
         </h1>
-        <p className="text-dark-light mt-4 text-center md:text-xl lg:text-left">
+        <p className="text-dark-light mt-4 text-center md:text-xl lg:text-base xl:text-xl lg:text-left">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua
         </p>
 
-        <div className="flex flex-col gap-y-2.5 mt-10 relative">
+        <div className="flex flex-col gap-y-2.5 mt-10 lg:mt-6 xl:mt-10 relative">
           <div className="relative">
             <FiSearch className=" absolute left-3 top-1/2 -translate-y-1/2 w-6 h-6 text-[#959EAD]" />
             <input
