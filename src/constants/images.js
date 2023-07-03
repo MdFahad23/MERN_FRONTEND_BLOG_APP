2e8e4ff549da73ba3417eed87e37a47b82b06a54
Md Fahad <fahadbdctglh8@gmail.com>
2023-07-03 14:58:00 +0600

    Created Cat component

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,12 +2,14 @@ import Logo from "../assets/Logo.svg";
 import HeroImage from "../assets/Hero.svg";
 import PostImage from "../assets/Posts/post1.jpg";
 import PostProfileImage from "../assets/Posts/post-profile.svg";
+import CatImage from "../assets/CatImage.svg";
 
 const images = {
   Logo,
   HeroImage,
   PostImage,
   PostProfileImage,
+  CatImage,
 };
 
 export default images;
