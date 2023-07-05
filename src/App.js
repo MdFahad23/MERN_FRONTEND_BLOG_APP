c030fe66978976a873c818bb25f01d8be3d3174b
Md Fahad <fahadbdctglh8@gmail.com>
2023-07-05 11:00:14 +0600

    Created SuggestedPost and edit every things

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+
 import Homepage from "./pages/home/Homepage";
+import ArticleDetailPage from "./pages/ArticleDetail/ArticleDetailPage";
 
 function App() {
   return (
     <div className="font-opensans">
-      <Homepage />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/blog/:id" element={<ArticleDetailPage />} />
+      </Routes>
     </div>
   );
 }
